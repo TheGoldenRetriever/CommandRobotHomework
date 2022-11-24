bfe00a71557992ed72dafeb6081736f21a17cff9
GoldenRetriever <74937228+TheGoldenRetriever@users.noreply.github.com>
2022-11-23 19:10:40 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.SpinningCommand;
+import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -20,7 +20,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveTrainSubsystem m_DriveTrainSubsystem = new DriveTrainSubsystem();
 
-  private final SpinningCommand m_autoCommand = new SpinningCommand(m_DriveTrainSubsystem);
+  private final SpinningCommand m_autoCommand = new SpinningeCommand(m_DriveTrainSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,6 +43,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return SpinningCommand.java;
+    return m_autoCommand;
   }
 }
